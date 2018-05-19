96f7e3e7513b041f1f10403cd5fdad11142b680c
Idorenyin Obong <idee4ril@gmail.com>
2018-05-19 19:39:11 +0100

    starter project

---
+++ app/src/main/java/com/raywenderlich/android/raynews/TopNewsActivity.kt
@@ -0,0 +1,19 @@
+package com.raywenderlich.android.raynews
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.raywenderlich.android.raynews.ui.topnews.TopNewsFragment
+
+class TopNewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.top_news_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, TopNewsFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/raywenderlich/android/raynews/ui/topnews/TopNewsViewModel.kt
@@ -0,0 +1,7 @@
+package com.raywenderlich.android.raynews.ui.topnews
+
+import android.arch.lifecycle.ViewModel
+
+class TopNewsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
